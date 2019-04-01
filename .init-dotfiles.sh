e33f686e33a1cfa96306cf3831ed27543d22c404
puckv <49120274+puckv@users.noreply.github.com>
2019-04-01 10:22:13 +0200

    File permissions and tarsnapper cronjob changes

--- .init-dotfiles.sh
+++ .init-dotfiles.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+# Restrictive permissions for sensitive files
+chmod 700 ~/.gnupg ~/.ssh
+chmod 600 ~/.ssh/*
+
 # Replace tarsnapper.yml to use absolute paths
 sed -i.bak "s#~/#${HOME}/#g" ~/.tarsnap/tarsnapper.yml
 rm ~/.tarsnap/tarsnapper.yml.bak
 
 # Install tarsnapper cron entry
-CRON_ENTRY='52 * * * * PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin tarsnapper -c $HOME/.tarsnap/tarsnapper.yml make'
+CRON_ENTRY='04 * * * * PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin tarsnapper -c $HOME/.tarsnap/tarsnapper.yml make >/dev/null 2>&1 || echo "Tarsnapper failed"'
 if ! crontab -l >/dev/null 2>&1; then
     echo "$CRON_ENTRY" | crontab -
 else
